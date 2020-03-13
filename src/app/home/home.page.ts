d13396c12c145589b60388b141a98c41c926a6af
duyan900 <58454434+duyan900@users.noreply.github.com>
2020-03-13 13:41:08 +0700

    add missing

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { BluetoothLE } from '@ionic-native/bluetooth-le/ngx';
+import { ToastController, Platform } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +8,86 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public bluetoothle: BluetoothLE, public toastController: ToastController, public plt: Platform) {
 
+
+
+
+    // constructor(public bluetoothle: BluetoothLE, public plt: Platform) {
+
+    this.plt.ready().then((readySource) => {
+
+      console.log('Platform ready from', readySource);
+
+      this.bluetoothle.initialize().then(ble => {
+        console.log('ble', ble.status) // logs 'enabled'
+      });
+
+    });
+  }
+  ngOnInit() {
+    // this.initialize();
+  }
+  initialize() {
+    // this.ble.initialize().subscribe(result => {
+    //   console.log(`bluetooth status: ${result.status}`)
+    //   this.scan()
+    // })
+  }
+
+  scan() {
+
+
+    // let scanSubscription = this.ble.startScan({
+    //   // "services": [],
+    //   // "allowDuplicates": true,
+    //   // "scanMode": this.ble.SCAN_MODE_LOW_LATENCY,
+    //   // "matchMode": this.ble.MATCH_MODE_AGGRESSIVE,
+    //   // "matchNum": this.ble.MATCH_NUM_MAX_ADVERTISEMENT,
+    //   // "callbackType": this.ble.CALLBACK_TYPE_ALL_MATCHES,
+    // }).subscribe(result => {
+    //   console.log(JSON.stringify(result))
+    //   if (result.status === 'scanResult') {
+    //     // if (result.name == this.reservation.bluetooth_address) {
+    //     // clearTimeout(scanTimeout)
+    //     this.presentToast(result);
+    //     this.ble.stopScan().then(() => {
+    //       // console.log('scan Stopped 1')
+    //       // let device: GocarDevice = {
+    //       //   address: result.address,
+    //       //   characteristic: this.reservation.bluetooth_characteristic,
+    //       //   service: this.reservation.bluetooth_service
+    //       // }
+    //       // this.connect2(device)
+    //       scanSubscription.unsubscribe()
+    //     })
+    //     // }
+    //   } else if (result.status === 'scanStarted') {
+    //     console.log('scan started')
+    //     this.presentToast('scan started');
+    //   } else {
+    //     // TODO handle error
+    //     // this.isDisabledBluetooth
+    //     console.log('start scan error: ' + JSON.stringify(result))
+    //     this.presentToast(result);
+    //   }
+    // }, err => {
+    //   console.log('start scan error 1', err)
+    //   this.presentToast(err.message);
+    //   // this.gocarToast.showToast({ message: err.message })
+    //   // this.isDisabledBluetooth = false
+    //   // this.isBluetooth = false
+    //   // setTimeout(() => {
+    //   //   this.isBluetooth = false
+    //   // })
+    //   // clearTimeout(scanTimeout)
+    // })
+  }
+  async presentToast(msg) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000
+    });
+    toast.present();
+  }
 }
